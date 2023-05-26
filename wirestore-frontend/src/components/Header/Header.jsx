1f55fd8f7a69c69395a049ce285b9e558bb67fcd
g-sillva <silvagabriel.limars@gmail.com>
2023-05-26 07:22:21 -0300

    Frontend - Added reset css & global variables

--- wirestore-frontend/src/components/Header/Header.jsx
+++ wirestore-frontend/src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
+import './Header.scss';
 
 function Header() {
-  return <div>header</div>;
+  return <div className="teste">header</div>;
 }
 
 export default Header;
--- wirestore-frontend/src/routes/AppRoutes.jsx
+++ wirestore-frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { ROUTES } from "./routesConstants";
-import HomePage from "../pages/HomePage";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ROUTES } from './routesConstants';
+import HomePage from '../pages/HomePage';
 
 function AppRoutes() {
   return (
